81ddf11aada75d5545960f4ec419d5e439ebf2e9
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-05-22 16:54:08 +0000

    js

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@
 async function updateWeather() {
   try {
     const infoJSON = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
-    const kelvinTemp = await infoJSON.main[1]
-    const weatherImage = await infoJSON.weather[4]
+    const kelvinTemp = await infoJSON.main.temp
+    const weatherImage = await infoJSON.weather.icon
     const celsiusTemp = kelvinTemp + 273.15
     document.getElementById("answer").innerHTML = 
       "<p>" + celsiusTemp + "°C</p>" + "<img src=" + weatherImage + "/>"
